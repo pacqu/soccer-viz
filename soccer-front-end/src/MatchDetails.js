7d50fa61b57f1d67b7253eaff754860fe167e180
Justin Pacquing <pacqqu@gmail.com>
2022-06-10 14:14:42 -0400

    adding team lineups

--- soccer-front-end/src/MatchDetails.js
+++ soccer-front-end/src/MatchDetails.js
@@ -11,64 +11,44 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import TeamDetails from './TeamDetails';
 
 function MatchDetails(props) {
   const details = props.details
   const players = props.players
   const team1Formation = JSON.parse(details['team1.formation'])
-  const team1Lineup = team1Formation.lineup.map(player => players[player.playerId])
   const team2Formation = JSON.parse(details['team2.formation'])
-  const team2Lineup = team2Formation.lineup.map(player => players[player.playerId])
+  const team1 = {
+    formation: team1Formation,
+    name: details['team1.name']
+  }
+  const team2 = {
+    formation: team2Formation,
+    name: details['team2.name']
+  }
+  const team1Home = details['time1.side'] === 'home'
+  const homeTeam = team1Home ? team1 : team2 
+  const awayTeam = team1Home ? team2 : team1
   return (
     <Grid item xs={11} sx={{ marginBottom: "15px" }}>
       <Card sx={{ width: '100%' }}>
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+          <Typography gutterBottom variant="h3" component="div">
             {details.label}
           </Typography>
-          <Typography gutterBottom variant="body1" component="div">
+          <Typography gutterBottom variant="h4" component="div">
             Date: {details.date}
           </Typography>
-          <Typography gutterBottom variant="body1" component="div">
+          <Typography gutterBottom variant="h4" component="div">
             Venue: {details.venue}
           </Typography>
           <Grid container>
             <Grid item xs={5}>
-              <TableContainer component={Paper}>
-                <Table size="small" aria-label="a dense table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Name</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {team1Lineup.map(player => (
-                      <TableRow>
-                        <TableCell component="th" scope="row">{`${player.firstName} ${player.lastName}`}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
+              <TeamDetails team={homeTeam} home={true} players={players}/>
             </Grid>
-            <Grid item xs={2}></Grid>
+            <Grid item xs={2 }></Grid>
             <Grid item xs={5}>
-              <TableContainer component={Paper}>
-                <Table size="small" aria-label="a dense table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Name</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {team2Lineup.map(player => (
-                      <TableRow>
-                        <TableCell component="th" scope="row">{`${player.firstName} ${player.lastName}`}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
+              <TeamDetails team={awayTeam} home={false} players={players}/>
             </Grid>
           </Grid>
         </CardContent>
---
+++ soccer-front-end/src/TeamDetails.js
@@ -0,0 +1,68 @@
+import './App.css';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function TeamDetails(props) {
+  const team = props.team
+  const players = props.players
+  console.log(team)
+  const lineup = team.formation.lineup.map(player => players[player.playerId])
+  const bench = team.formation.bench.map(player => players[player.playerId])
+  return (
+    <>
+      <Typography gutterBottom variant="h5" component="div">
+        {team.name}
+      </Typography>
+      <Typography gutterBottom variant="h6" component="div">
+        Starting Lineup
+      </Typography>
+      <TableContainer component={Paper}>
+        <Table size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {lineup.map(player => (
+              <TableRow>
+                <TableCell component="th" scope="row">{`${player.firstName} ${player.lastName}`}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Typography gutterBottom variant="h6" component="div">
+        Bench
+      </Typography>
+      <TableContainer component={Paper}>
+        <Table size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {bench.map(player => (
+              <TableRow>
+                <TableCell component="th" scope="row">{`${player.firstName} ${player.lastName}`}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
+
+export default TeamDetails;
