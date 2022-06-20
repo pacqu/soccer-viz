3089e9a4f0d3208cae6edccc9347dc4082aaeaab
Justin Pacquing <pacqqu@gmail.com>
2022-06-20 14:41:46 -0400

    added positional data to table/formation graphs

--- soccer-front-end/src/FormationGraph.js
+++ soccer-front-end/src/FormationGraph.js
@@ -3,6 +3,12 @@ import * as React from 'react';
 import { Graph, DefaultLink, DefaultNode } from '@visx/network';
 
 function FormationGraph(props) {
+  const posColors = {
+    GK: 'red',
+    DF: 'orange',
+    MD: 'yellow',
+    FW: 'blue'
+  }
   const width = props.width
   const height = props.height
   const team = props.team
@@ -12,12 +18,37 @@ function FormationGraph(props) {
   const bench = team.formation.bench.map(player => players[player.playerId])
   const nodes = team.formation.lineup.map(player => {
     const player_position = positions[player.playerId]
-    if (props.home) return { x: player_position.start_x * (width/100), y: player_position.start_y * (height/100) }
-    else return { x: width - (player_position.start_x * (width / 100)), y: height - (player_position.start_y * (height / 100)) }
+    const posCode = JSON.parse(players[player.playerId]['role'])['code2']
+     return { 
+       x: player_position.start_x * (width / 100), 
+       y: player_position.start_y * (height / 100),
+       color: posColors[posCode]
+    }
+    {/*if (props.home) return { x: width - (player_position.start_x * (width / 100)), y: height - (player_position.start_y * (height / 100)) }*/}
   })
   const dataSample = { nodes, links: [] }
+  console.log(dataSample)
   return (
-    <svg width={width} height={height}><Graph graph={dataSample} linkComponent={DefaultLink} nodeComponent={DefaultNode} /></svg>
+    <svg viewBox="0 0 1150 780" xmlns="http://www.w3.org/2000/svg" >
+      <path d="M0 0h1150v780H0z" className="turf" />
+      <path className="line" d="M575 730V50h525v680H50V50h525" />
+      <circle cx={575} cy={390} className="line" r={91.5} />
+      <circle cx={575} cy={390} className="dot" />
+      <circle cx={160} cy={390} className="dot" />
+      <circle cx={990} cy={390} className="dot" />
+      <path
+        d="M251.5 390a91.5 91.5 0 0 0-36.5-73.4m36.5 73.4a91.5 91.5 0 0 1-36.5 73.4M898.5 390a91.5 95.5 0 0 0 36.5 73.4M898.5 390a91.5 95.5 0 0 1 36.5-73.4M50 60q10 0 10-10m1030 0q0 10 10 10M50 720q10 0 10 10m1030 0q0-10 10-10"
+        className="line"
+      />
+      <path className="goal" d="M50 426.6H35v-73.2h15M1100 426.6h15v-73.2h-15" />
+      <path
+        className="line"
+        d="M50 481.6h55V298.4H50v293.2h165V188.4H50M1100 481.6h-55V298.4h55v293.2H935V188.4h165"
+      />
+      <svg width={width} height={height}><Graph graph={dataSample} linkComponent={DefaultLink} nodeComponent={({ node: { color } }) =>
+          color ? <DefaultNode fill={color} /> : <DefaultNode />
+        } /></svg>
+    </svg>
   );
 }
 
--- soccer-front-end/src/TeamDetails.js
+++ soccer-front-end/src/TeamDetails.js
@@ -16,11 +16,12 @@ import { ParentSize } from '@visx/responsive';
 import FormationGraph from './FormationGraph';
 
 function TeamDetails(props) {
+  const posOrder = {'GK': 1, 'DF': 2, 'MD': 3, 'FW': 4}
   const team = props.team
   const players = props.players
   const positions = props.positions
-  const lineup = team.formation.lineup.map(player => players[player.playerId])
-  const bench = team.formation.bench.map(player => players[player.playerId])
+  const lineup = team.formation.lineup.map(player => ({posCode: JSON.parse(players[player.playerId]['role'])['code2'], posNum: posOrder[JSON.parse(players[player.playerId]['role'])['code2']], ...players[player.playerId]})).sort((a,b) => a.posNum - b.posNum)
+  const bench = team.formation.bench.map(player => ({posCode: JSON.parse(players[player.playerId]['role'])['code2'], posNum: posOrder[JSON.parse(players[player.playerId]['role'])['code2']], ...players[player.playerId]})).sort((a,b) => a.posNum - b.posNum)
   const nodes = team.formation.lineup.map(player => {
     const player_position = positions[player.playerId]
     if (props.home) return { x: player_position.start_x * 10.1, y: player_position.start_y * 6.8 }
@@ -41,13 +42,15 @@ function TeamDetails(props) {
               <Table size="small" aria-label="a dense table">
                 <TableHead>
                   <TableRow>
+                    <TableCell>Position</TableCell>
                     <TableCell>Name</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
                   {lineup.map(player => (
                     <TableRow>
-                      <TableCell component="th" scope="row">{`${player.firstName} ${player.lastName}`}</TableCell>
+                      <TableCell component="th" scope="row">{`${player.posCode} `}</TableCell>
+                      <TableCell>{`${player.firstName} ${player.lastName}`}</TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
@@ -62,13 +65,15 @@ function TeamDetails(props) {
               <Table size="small" aria-label="a dense table">
                 <TableHead>
                   <TableRow>
+                    <TableCell>Position</TableCell>
                     <TableCell>Name</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
                   {bench.map(player => (
                     <TableRow>
-                      <TableCell component="th" scope="row">{`${player.firstName} ${player.lastName}`}</TableCell>
+                      <TableCell component="th" scope="row">{`${player.posCode} `}</TableCell>
+                      <TableCell>{`${player.firstName} ${player.lastName}`}</TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
@@ -76,24 +81,7 @@ function TeamDetails(props) {
             </TableContainer>
           </Grid>
           <Grid container>
-            <svg viewBox="0 0 1150 780" xmlns="http://www.w3.org/2000/svg" >
-              <path d="M0 0h1150v780H0z" className="turf" />
-              <path className="line" d="M575 730V50h525v680H50V50h525" />
-              <circle cx={575} cy={390} className="line" r={91.5} />
-              <circle cx={575} cy={390} className="dot" />
-              <circle cx={160} cy={390} className="dot" />
-              <circle cx={990} cy={390} className="dot" />
-              <path
-                d="M251.5 390a91.5 91.5 0 0 0-36.5-73.4m36.5 73.4a91.5 91.5 0 0 1-36.5 73.4M898.5 390a91.5 95.5 0 0 0 36.5 73.4M898.5 390a91.5 95.5 0 0 1 36.5-73.4M50 60q10 0 10-10m1030 0q0 10 10 10M50 720q10 0 10 10m1030 0q0-10 10-10"
-                className="line"
-              />
-              <path className="goal" d="M50 426.6H35v-73.2h15M1100 426.6h15v-73.2h-15" />
-              <path
-                className="line"
-                d="M50 481.6h55V298.4H50v293.2h165V188.4H50M1100 481.6h-55V298.4h55v293.2H935V188.4h165"
-              />
               <FormationGraph home={props.home} width={1150} height={780} team={team} players={players} positions={positions} />
-            </svg>
           </Grid>
 
         </Grid>
